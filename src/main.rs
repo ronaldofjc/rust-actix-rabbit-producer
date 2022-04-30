22556f3ae1d51f5ae7222d98d0321ac049f0eb94
Ronaldo Feitosa <ronaldo.feitosa@mercadolivre.com>
2022-04-30 15:14:17 -0300

    Update project

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,7 @@ mod repository;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
-        .init();
+    tracing_subscriber::fmt().with_max_level(tracing::Level::INFO).init();
     dotenv::dotenv().ok();
     let port = std::env::var("PORT").unwrap_or("8090".to_string());
     let address = format!("127.0.0.1:{}", port);
